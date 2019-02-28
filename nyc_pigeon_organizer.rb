e3efe2b02816f4d43d97059a079abba5832af7ac
mrsalvini <dsgmcosta@gmail.com>
2019-02-28 17:39:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  new_hash = {}
+
+  data.each { |trait, subtraits_hash|
+    subtraits_hash.each { |subtrait, names_arr|
+      names_arr.each { |name|
+        if !new_hash.member?(name)
+          new_hash[name] = {}
+        end
+      }
+    }
+
+    new_hash.each { |name, traits_hash|
+      new_hash[name][trait] = []
+    }
+  }
+
+  new_hash.each { |name, traits_hash|
+    data.each { |trait, subtraits_hash|
+      subtraits_hash.each { |subtrait, names_arr|
+        if names_arr.include?(name)
+          traits_hash[trait] << subtrait.to_s
+        end
+      }
+    }
+  }
+
+  puts new_hash
+  new_hash
 end
