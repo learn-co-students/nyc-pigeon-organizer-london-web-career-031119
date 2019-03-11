a93be270f26dcc809cee131606ec2e86924dc895
Peter Keller <keller.f.peter@gmail.com>
2019-03-11 22:49:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |i, j|
+      j.each do |name|
+        if !new_hash.key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].key?(key)
+          new_hash[name][key] = []
+        end
+
+        if !new_hash[name][key].include?(i)
+          new_hash[name][key].push(i.to_s)
+        end
+      end
+    end
+  end
+  new_hash
 end
