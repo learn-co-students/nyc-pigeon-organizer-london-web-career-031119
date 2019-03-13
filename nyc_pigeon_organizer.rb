983dcb0fa52e14e95f74f9274a2a6ef9484f56b4
github username <github email address>
2019-03-13 10:46:41 +0000

    Submit

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |data_type, data_set|
+    data_set.each do |data_info, names|
+      names.each do |name|
+        if pigeon_list.include?(name)
+          pigeon_list[name].include?(data_type) ? pigeon_list[name][data_type] << data_info.to_s : pigeon_list[name][data_type] = Array(data_info.to_s)
+        else
+          pigeon_list[name] = {data_type => Array(data_info.to_s)}
+        end
+      end
+    end
+  end
+  binding.pry
+  pigeon_list
 end
